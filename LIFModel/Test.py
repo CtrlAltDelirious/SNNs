6c350d36edb79c3ed2791f6a05ded5884ca45fc7
CtrlAltDelirious <76526614+CtrlAltDelirious@users.noreply.github.com>
2025-06-17 18:41:15 +0100

    Update Test.py

--- LIFModel/Test.py
+++ LIFModel/Test.py
@@ -2,7 +2,7 @@ import brian2 as b2
 from SNNs.LIFModel import Main
 from SNNs.tools import input_factory
 
-print("resting potential: {}".format(LIFModel.V_REST))
+print("resting potential: {}".format(Main.V_REST))
 
 # create a step current with amplitude = I_min
 step_current = input_factory.get_step_current(
@@ -11,7 +11,7 @@ step_current = input_factory.get_step_current(
 
 # run the LIF model.
 # Note: As we do not specify any model parameters, the simulation runs with the default values
-(state_monitor,spike_monitor) = LIFModel.simulate_LIF_neuron(input_current=step_current, simulation_time = 100 * b2.ms)
+(state_monitor,spike_monitor) = Main.simulate_LIF_neuron(input_current=step_current, simulation_time = 100 * b2.ms)
 
 # plot I and vm
 plot_tools.plot_voltage_and_current_traces(
