b1b9263076dbb84083d4102864d31cc87e712aea
CtrlAltDelirious <76526614+CtrlAltDelirious@users.noreply.github.com>
2025-06-18 14:56:35 +0100

    Update Test.py

--- LIFModel/Test.py
+++ LIFModel/Test.py
@@ -1,13 +1,16 @@
 import brian2 as b2
+from brian2 import uamp
+import sys
+sys.path.append("C:/Users/zafir/OneDrive/PC_STUFF/Desktop/Queen_Mary/Research/Code")
 from SNNs.LIFModel import Main
 from SNNs.tools import input_factory
-
+from SNNs.tools import plot_tools
 print("resting potential: {}".format(Main.V_REST))
 
 # create a step current with amplitude = I_min
 step_current = input_factory.get_step_current(
     t_start=5, t_end=100, unit_time=b2.ms,
-    amplitude=2*10**-9)  # set I_min to your value
+    amplitude=0.002*uamp)  # set I_min to your value
 
 # run the LIF model.
 # Note: As we do not specify any model parameters, the simulation runs with the default values
